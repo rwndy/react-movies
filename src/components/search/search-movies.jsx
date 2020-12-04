8aa8c3b2a9096f3e956ca95dd95bd31fd908b121
riwandi <riwandy10@gmail.com>
2020-12-04 11:38:25 +0700

    styling search terms and animations

--- src/components/search/search-movies.jsx
+++ src/components/search/search-movies.jsx
@@ -1,9 +1,10 @@
 
 const SearchMovies = ({search, onSearch, getSubmit}) => {
   return (
-    <section>
-      <form onSubmit={getSubmit}>
-        <input type="text" value={search.searchTerms} onChange={onSearch} autoComplete="off" name="searchTerms"/>
+    <section className="section-search">
+      <form onSubmit={getSubmit} className="section-search--form">
+        <input type="text" value={search.searchTerms} onChange={onSearch} autoComplete="off" name="searchTerms" className="section-search--term" placeholder="Search your favourite film..."/>
+        <span className="section-search--bar"></span>
       </form>
     </section>
   )
