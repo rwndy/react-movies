6da9298f22a75e4f3862d7a590f0dc7fdf374d15
riwandi <riwandy10@gmail.com>
2020-12-06 20:42:24 +0700

    fixed bug

--- src/components/search/search-movies.jsx
+++ src/components/search/search-movies.jsx
@@ -3,7 +3,7 @@ const SearchMovies = ({search, onSearch, getSubmit}) => {
   return (
     <section className="section-search">
       <form onSubmit={getSubmit} className="section-search--form">
-        <input type="text" value={search} onChange={onSearch} autoComplete="off" name="searchTerms" className="section-search--term" placeholder="Search your favourite film..."/>
+        <input type="text" value={search} onChange={onSearch} autoComplete="off" className="section-search--term" placeholder="Search your favourite film..."/>
         <span className="section-search--bar"></span>
       </form>
     </section>
